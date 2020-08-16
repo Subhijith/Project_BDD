49b6f37dc8794290bfb4db4ffd06f452ab49d8c7
Subhijith <tsubhijith@gmail.com>
2020-08-16 19:02:45 +0800

    added maven execution commands

--- src/test/java/test_runner/Runner.java
+++ src/test/java/test_runner/Runner.java
@@ -1,6 +1,7 @@
 package test_runner;
 
 import org.junit.runner.RunWith;
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
@@ -9,13 +10,9 @@ import io.cucumber.junit.CucumberOptions;
 		features = "src/test/resources/features", //the path of the feature files
 		glue={"step_definations"}, //the path of the step definition files
 		plugin= {"pretty","html:target/HTMLreports/report.html", "json:target/json_output/cucumber.json", "junit:target/junit_xml/cucumber.xml"}, //to generate different types of reporting
-		monochrome = true, //display the console output in a proper readable format
-		strict = true, //it will check if any step is not defined in step definition file
-		dryRun = false //to check the mapping is proper between feature file and step def file
-		//tags = {"~@SmokeTest" , "~@RegressionTest", "~@End2End"}			
+		monochrome = true
+				
 		)
 public class Runner {
-	
-	
 
 }
