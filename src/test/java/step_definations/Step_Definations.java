f00146dfe9fb586c2a2d603d8af272d02d4acfb0
Subhijith <tsubhijith@gmail.com>
2020-08-22 00:00:05 +0800

    added more browser steps

--- src/test/java/step_definations/Step_Definations.java
+++ src/test/java/step_definations/Step_Definations.java
@@ -1,6 +1,10 @@
 package step_definations;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.cucumber.java.en.Given;
@@ -13,10 +17,17 @@ public class Step_Definations {
 
 	@Given("User is able to acess login page")
 	public void user_is_able_to_acess_login_page() {
+		try {
 	   System.out.println("Navigate to website URL..");
 	   String path = System.getProperty("user.dir");
 	   System.setProperty("webdriver.gecko.driver",path+"/geckodriver.exe");
 	   driver = new FirefoxDriver();
+	   driver.manage().window().maximize();
+	   driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	   
 	   
 	   
@@ -24,7 +35,26 @@ public class Step_Definations {
 
 	@Given("user is on login page")
 	public void user_is_on_login_page() {
+		try {
 		System.out.println("validate login page..");
+		driver.get("https://www.google.com/");
+		WebElement inputBox = driver.findElement(By.xpath("//input[@name=\"q\"]"));
+		boolean dis = inputBox.isDisplayed();
+		System.out.println("Is displayed: "+dis);
+		
+		inputBox.sendKeys("linkedin");
+		
+		WebElement linkedin = driver.findElement(By.xpath("//*[contains(text(),\"LinkedIn: Log In or Sign Up\")]"));
+		System.out.println("found text is :"+linkedin.getText());
+		Thread.sleep(5000);
+		
+		
+		
+		
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@When("user enters username and password")
